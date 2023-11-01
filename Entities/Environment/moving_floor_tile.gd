08d3bba7cf77c586676637931cf111651b7d0ea3
plexsoup <plexsoup@gmail.com>
2023-11-01 11:13:04 -0700

    fix bug in moving floor tiles

--- Entities/Environment/moving_floor_tile.gd
+++ Entities/Environment/moving_floor_tile.gd
@@ -16,11 +16,17 @@ func _ready():
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	if State == States.READY:
 		# move the ground image  in the direction of the camera, twice it's size.
-		
-		var distance = self.get_rect().size.x * 2.0
-		var direction = self.global_position.direction_to(get_viewport().get_camera_2d().global_position)
-		direction.y = 0
 
+		var tile_count = get_parent().get_child_count()
+		var distance = self.get_rect().size.x * float(tile_count)
+		var camera = get_viewport().get_camera_2d()
+		
+		var direction
+		if camera.global_position.x > self.global_position.x:
+			direction = Vector2.RIGHT
+		else:
+			direction = Vector2.LEFT
+		
 		State = States.MOVING
 		var tween = create_tween()
 		#tween.tween_property(self, "position", position + Vector2(0, 64), 0.5)
--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -1,16 +1,21 @@
 extends Control
 
+@export var game_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect_button_audio()
-	
+	connect_button_signals()
+
 
 func connect_button_audio():
 	for button in %Buttons.get_children():
 		button.mouse_entered.connect($Audio/HoverNoise.play)
 		button.pressed.connect($Audio/ClickNoise.play)
-		
+
+
+func connect_button_signals():
+	for button in %Buttons.get_children():
 		if "play" in button.name.to_lower():
 			button.pressed.connect(start_game)
 		elif "quit" in button.name.to_lower():
@@ -21,11 +26,19 @@ func connect_button_audio():
 
 func start_game():
 	await get_tree().create_timer(0.5).timeout # wait for click audio
-	get_tree().change_scene_to_file("res://Levels/placeholder/placeholder_2d_scene.tscn")
+	if game_scene != null:
+		get_tree().change_scene_to_packed(game_scene)
+	else:
+		printerr("Configuration error in main menu. game_scene parameter needs a packed scene.")
+		printerr(get_tree().current_scene.scene_file_path)
+		#get_tree().change_scene_to_file("res://Levels/placeholder/placeholder_2d_scene.tscn")
+
 
 func quit_game():
 	get_tree().quit()
 
+
 func view_settings():
 	await get_tree().create_timer(0.5).timeout # wait for click audio
 	get_tree().change_scene_to_file("res://Menus/settings_menu.tscn")
+
