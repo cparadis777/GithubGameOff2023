fb5425076caecc682fec5284fe018114ced445b5
plexsoup <plexsoup@gmail.com>
2023-11-02 15:53:55 -0700

    added placeholder run cycle to player

--- Menus/scene_launch_button.gd
+++ Menus/scene_launch_button.gd
@@ -8,12 +8,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
 
 func _on_pressed():
 	if scene != null:
