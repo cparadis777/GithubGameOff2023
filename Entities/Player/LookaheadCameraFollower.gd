416fed2c844cab236d661c5f29b6f07a36037adf
plexsoup <plexsoup@gmail.com>
2023-11-05 01:45:01 -0800

    trivial stuff. not important

--- Entities/Player/LookaheadCameraFollower.gd
+++ Entities/Player/LookaheadCameraFollower.gd
@@ -13,7 +13,7 @@ func _ready():
 	original_offset = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 	
 	#lookahead(delta)
