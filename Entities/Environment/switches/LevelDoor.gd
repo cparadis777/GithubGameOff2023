0ba19a1b9f9113bb02b3822fcc2aecff13eba899
Ian Cowan <icowan@ucsc.edu>
2023-11-07 14:23:43 -0800

    BaseLevel extends Node2D not BaseContainer
    level objects need not be children of the container node

--- Entities/Environment/switches/LevelDoor.gd
+++ Entities/Environment/switches/LevelDoor.gd
@@ -3,6 +3,8 @@ extends Area2D
 var player_near : bool = false
 var pressed : bool = false
 
+@export var next_scene:PackedScene
+
 signal toggled(pressed)
 
 # Called when the node enters the scene tree for the first time.
@@ -13,6 +15,8 @@ func _ready():
 func level_transition():
 	print_debug("level_transition()")
 	toggled.emit(pressed)
+	# some kind of transition animation?
+	StageManager.change_scene_to(next_scene)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if !pressed and Input.is_action_just_pressed("interact") and player_near:
