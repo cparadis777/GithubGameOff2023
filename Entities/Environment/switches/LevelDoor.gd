99bd0dee3d61ac92ba6d7e8e18c8a244a8bd01de
Ian Cowan <icowan@ucsc.edu>
2023-11-08 14:48:18 -0800

    container transition animation started

--- Entities/Environment/switches/LevelDoor.gd
+++ Entities/Environment/switches/LevelDoor.gd
@@ -1,10 +1,19 @@
 extends Area2D
 
-var player_near : bool = false
-var pressed : bool = false
+#Defines whether an exit is a horizontal doorway vertical ladder/hatch
+enum DoorOrientation {HORIZONTAL, VERTICAL}
 
 @export var next_scene:PackedScene
+# how long to wait on a b screen before next scene
 @export_range(0.0, 2.0, 0.1) var wait_seconds:float = 0.0
+# how long this side of the transition animation will take
+@export_range(0.0, 2.0, 0.1) var transition_seconds:float = 0.5
+@export var unlocked:bool = true;
+@export var orientation:DoorOrientation = DoorOrientation.HORIZONTAL
+@export var location:Utils.Directions
+
+var player_near : bool = false
+var pressed : bool = false
 
 signal toggled(pressed)
 
@@ -13,15 +22,27 @@ func _ready():
 	$Label.hide();
 	pass # Replace with function body.
 
-func level_transition():
-	print_debug("level_transition()")
+func exit_level():
+	print_debug("exit_level()")
+	# temporarily disable input and world physics, but next level must re-enable
+
+	get_tree().get_root().set_process_input(false)
+
 	toggled.emit(pressed)
+	StageManager.current_player.play_run_animation()
 	SceneTransition.change_scene(next_scene, wait_seconds)
+	await get_tree().create_timer(wait_seconds + transition_seconds).timeout
+	
+
+# Should be done on load of a level
+func enter_level():
+	pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if !pressed and Input.is_action_just_pressed("interact") and player_near:
+	if Input.is_action_just_pressed("interact") and player_near and unlocked:
 		pressed = true;
-		level_transition()
+		exit_level()
 
 func _on_body_entered(body):
 	if body == StageManager.current_player:
--- Utilities/UtilityScenes/SceneTransition.gd
+++ Utilities/UtilityScenes/SceneTransition.gd
@@ -3,10 +3,11 @@ extends CanvasLayer
 # Scene Transition method from this video: 
 # https://www.youtube.com/watch?v=yZQStB6nHuI&ab_channel=TheShaggyDev
 
-func change_scene(scene: PackedScene, wait_seconds: float = 0.0) -> void:
+func change_scene(scene: PackedScene, anim_length: float = 0.5, hold: float = 0.0) -> void:
+	$AnimationPlayer.get_animation("dissolve").length = anim_length
 	$AnimationPlayer.play("dissolve")
 	await $AnimationPlayer.animation_finished
-	if (wait_seconds > 0):
-		await get_tree().create_timer(wait_seconds).timeout
+	if (hold > 0):
+		await get_tree().create_timer(hold).timeout
 	StageManager.change_scene_to(scene)
 	$AnimationPlayer.play_backwards("dissolve")
