f90ae5c73648e4f8417b4ba92a692efb46aa4212
plexsoup <plexsoup@gmail.com>
2023-11-11 01:47:47 -0800

    player_dead end game screen

--- Utilities/UtilityScripts/StageManager.gd
+++ Utilities/UtilityScripts/StageManager.gd
@@ -4,6 +4,7 @@ var current_level
 var current_player
 
 var beat_the_boss_screen : PackedScene = preload("res://CutScenes/BeatTheBoss.tscn")
+var player_dead_scene: PackedScene = preload("res://CutScenes/PlayerDead.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +18,14 @@ func change_scene_to(scene):
 
 func _on_NPC_died(npc_name):
 	if "boss" in npc_name.to_lower():
-		end_game()
-		
-func end_game():
-	change_scene_to(beat_the_boss_screen)
+		end_game("win")
+
+func _on_player_dead_and_buried():
+	end_game("lose")
+	
+func end_game(status):
+	match status:
+		"win":
+			change_scene_to(beat_the_boss_screen)
+		"lose":
+			change_scene_to(player_dead_scene)
