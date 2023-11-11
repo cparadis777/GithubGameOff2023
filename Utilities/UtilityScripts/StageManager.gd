9d0364c568f457bb25c5aeb8acb7c695a0f44461
plexsoup <plexsoup@gmail.com>
2023-11-11 01:25:11 -0800

    end-game scene for beating the boss.

--- Utilities/UtilityScripts/StageManager.gd
+++ Utilities/UtilityScripts/StageManager.gd
@@ -3,6 +3,7 @@ extends Node
 var current_level
 var current_player
 
+var beat_the_boss_screen : PackedScene = preload("res://CutScenes/BeatTheBoss.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,10 @@ func change_scene_to(scene):
 	if scene is PackedScene:
 		# add some animated transitions later.
 		get_tree().change_scene_to_packed(scene)
+
+func _on_NPC_died(npc_name):
+	if "boss" in npc_name.to_lower():
+		end_game()
+		
+func end_game():
+	change_scene_to(beat_the_boss_screen)
