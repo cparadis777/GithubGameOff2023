9eea0fd3d51df017edcbd966c961ab1e12615354
Ian Cowan <icowan@ucsc.edu>
2023-11-13 20:53:09 -0800

    ContainerData exports exit flags for ease of use

--- Levels/BaseLevel.gd
+++ Levels/BaseLevel.gd
@@ -3,21 +3,19 @@ extends Node2D
 @export var level_width: int = 384
 @export var level_height: int = 192
 
-enum dir_flags {LEFT = 1, RIGHT = 2, UP = 4, DOWN = 8}
-
 @export_flags("Left", "Right", "Up", "Down") var exit_flags:int = 0
-@export var enter_from: dir_flags
+@export var enter_from:= Utils.Directions.LEFT
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var directions = []
-	if (exit_flags & dir_flags.LEFT):
+	if (exit_flags & Utils.DirectionFlags.LEFT):
 		directions.push_back(Utils.Directions.LEFT)
-	if (exit_flags & dir_flags.RIGHT):
+	if (exit_flags & Utils.DirectionFlags.RIGHT):
 		directions.push_back(Utils.Directions.RIGHT)
-	if (exit_flags & dir_flags.UP):
+	if (exit_flags & Utils.DirectionFlags.UP):
 		directions.push_back(Utils.Directions.UP)
-	if (exit_flags & dir_flags.DOWN):
+	if (exit_flags & Utils.DirectionFlags.DOWN):
 		directions.push_back(Utils.Directions.DOWN)
 	$BaseContainer.set_entrances(directions)
 	
--- Utilities/UtilityScenes/ContainerProperties.gd
+++ Utilities/UtilityScenes/ContainerProperties.gd
@@ -4,8 +4,20 @@ class_name ContainerData
 
 
 @export var container_type:ContainerProperties.container_type
-@export var container_exits:Dictionary = {Utils.Directions.UP: false, Utils.Directions.RIGHT: false, Utils.Directions.DOWN: false, Utils.Directions.LEFT: false}
+@export_flags("Left", "Right", "Up", "Down") var container_exit_flags:int = Utils.DirectionFlags.NIL
 @export var scene_path:String
+
+var container_exits:Dictionary = {Utils.Directions.UP: false, Utils.Directions.RIGHT: false, Utils.Directions.DOWN: false, Utils.Directions.LEFT: false}
+
+func _ready():
+	if (container_exit_flags & Utils.DirectionFlags.LEFT):
+		container_exits[Utils.Directions.LEFT] = true;
+	if (container_exit_flags & Utils.DirectionFlags.RIGHT):
+		container_exits[Utils.Directions.RIGHT] = true;
+	if (container_exit_flags & Utils.DirectionFlags.UP):
+		container_exits[Utils.Directions.UP] = true;
+	if (container_exit_flags & Utils.DirectionFlags.DOWN):
+		container_exits[Utils.Directions.DOWN] = true;
 
 func match_exits(exits:Dictionary) -> bool:
 	for exit in exits:
