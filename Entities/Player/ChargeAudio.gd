5a97c62e809e89733561a51217de077912fba8ae
plexsoup <plexsoup@gmail.com>
2023-11-17 14:47:43 -0800

    add ChargeAudio.gd for PlayerController.
    allow three charge noises before strong punch

---
+++ Entities/Player/ChargeAudio.gd
@@ -0,0 +1,27 @@
+
+extends Node2D
+
+var charge_level : int = 0
+var max_charge : int = 2
+
+signal finished()
+
+func start():
+	$ChargeAudioSequence.play()
+	$Duration_Per_Level.start()
+
+func play():
+	start()
+
+
+func stop():
+	$ChargeAudioSequence.stop()
+
+
+func _on_duration_per_level_timeout():
+	if charge_level < max_charge:
+		$ChargeAudioSequence.stop()
+		$ChargeAudioSequence.play()
+		$Duration_Per_Level.start()
+		charge_level += 1
+	
