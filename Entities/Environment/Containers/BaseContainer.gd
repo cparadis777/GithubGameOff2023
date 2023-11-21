26870e25a69c2faef149710774c382acacec6f19
Ian Cowan <icowan@ucsc.edu>
2023-11-20 16:39:26 -0800

    large containers integrated into catalog & GeneratedPlayspace

--- Entities/Environment/Containers/BaseContainer.gd
+++ Entities/Environment/Containers/BaseContainer.gd
@@ -1,12 +1,12 @@
 extends StaticBody2D
 
-@export var enter_left = false
-@export var enter_right = false
-@export var enter_up = false
-@export var enter_down = false
+# note: order of exit flags must match the order in Utils.DirectionFlags
+@export_flags("LEFT", "RIGHT", "UP", "DOWN") var container_exit_flags:int = 15
+@onready var exits_container_node
 
-@export var doors_open = false
+@export var doors_unlocked = true
 @export var open_doors_with_switch = false
+@export var show_exterior_on_start = true
 
 var weigth:int = 100
 var grid_position:Vector2 = Vector2(100,100)
@@ -27,46 +27,49 @@ var has_entrance = {
 }
 
 @onready var doors = {
-	Utils.Directions.LEFT: $Doors/LEFT,
-	Utils.Directions.UP: $Doors/UP,
-	Utils.Directions.RIGHT: $Doors/RIGHT,
-	Utils.Directions.DOWN: $Doors/DOWN,
+	Utils.Directions.LEFT: $Exits/LEFT,
+	Utils.Directions.UP: $Exits/UP,
+	Utils.Directions.RIGHT: $Exits/RIGHT,
+	Utils.Directions.DOWN: $Exits/DOWN,
 }
 
 # entrance_arrows:Dictionary = {Utils.Directions.LEFT: null}
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	$Exterior.visible = true;
+	if (show_exterior_on_start):
+		$Exterior.visible = true
+	else:
+		$Exterior.visible = false
 	
-	if enter_left:
-		has_entrance[Utils.Directions.LEFT] = true
-		# left_wall.disabled = true
-		$Openings/Left.visible = true;
-	if enter_right:
-		has_entrance[Utils.Directions.RIGHT] = true
-		# right_wall.disabled = true
-		$Openings/Right.visible = true;
-	if enter_up:
-		has_entrance[Utils.Directions.UP] = true
-		# top_wall.disabled = true
-		$Openings/Top.visible = true;
-	if enter_down:
-		has_entrance[Utils.Directions.DOWN] = true
-		# bottom_wall.disabled = true
-		$Openings/Bottom.visible = true;
-		
-	if doors_open:
+	if (has_node("Exits")):
+		exits_container_node = $Exits
+	else:
+		print_debug("using deprecated BaseContainer. Use res://Entities/Environment/Containers/LargeContainer.tscn instead")
+	
+	remove_unneeded_doors()
+	
+	if doors_unlocked:
 		open_all_doors()
+
+func remove_unneeded_doors():
+	if container_exit_flags < 15:
+		print("found a container to check.")
+
+	for exit in exits_container_node.get_children():
+		# bitwise comparison math based on the name of the exit
+		# exits must be named LEFT, RIGHT, UP, or DOWN for this to work
+		if Utils.DirectionFlags[exit.name] & container_exit_flags:
+			has_entrance[Utils.flag_to_direction(Utils.DirectionFlags[exit.name])] = true;
+			exit.visible = true
+		else:
+			exit.queue_free()
 		
 func open_door(side:Utils.Directions):
 	# change the sprite?
 	# door_sprites[side]
 	if has_entrance[side]:
-		walls[side].disabled = true
-		if has_node("Doors"):
-				doors[side].locked = false
-				pass
+		doors[side].locked = false
 
 func open_all_doors():
 	# when room is completed, we can open all available doors
@@ -75,11 +78,7 @@ func open_all_doors():
 	# adjacent doors
 	print("container beaten")
 	for side in has_entrance:
-		if has_entrance[side]:
-			walls[side].disabled = true
-			if has_node("Doors"):
-				doors[side].locked = false
-				pass
+		doors[side].locked = false
 			
 
 func _on_switch_toggled(pressed):
