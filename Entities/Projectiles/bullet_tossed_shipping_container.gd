f61602a6bc21b88b2afe9fc8bb3d9aae51587912
plexsoup <plexsoup@gmail.com>
2023-11-27 18:28:21 -0800

    update collision mask on vending machine and crates

---
+++ Entities/Projectiles/bullet_tossed_shipping_container.gd
@@ -0,0 +1,30 @@
+extends "res://Entities/Projectiles/BulletBasic.gd"
+
+enum container_types { BENGAL, ICE, EAGLE }
+@export var types : container_types = container_types.BENGAL
+var textures = {
+	container_types.BENGAL: preload("res://art/low_res_containers/shipping_container_bengal_security-LOWRES.png"),
+	container_types.ICE: preload("res://art/low_res_containers/shipping_container_ice_and_freezy_LOWRES.png"),
+	container_types.EAGLE : preload("res://art/low_res_containers/shipping_container_ill_eagle-LOWRES.png"),
+} 
+var type : container_types
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	type = container_types.values().pick_random()
+	$ContainerExteriorSprite.texture = textures[type]
+
+
+func _on_body_entered(body):
+	#super(body) # cause damage to player
+	if body.is_in_group("Player"):
+		# crack open and spawn enemies
+		spawn_pieces()
+		
+func spawn_pieces():
+	var pieces = preload("res://Entities/Projectiles/broken_container_pieces.tscn").instantiate()
+	pieces.type = type
+	get_tree().get_root().add_child(pieces)
+	pieces.global_position = global_position
+	pieces.global_rotation = global_rotation
+	
--- Levels/BossFight/BossFight.gd
+++ Levels/BossFight/BossFight.gd
@@ -10,7 +10,10 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel") and $AnimationPlayer.current_animation != "":
+		$AnimationPlayer.seek(15.0)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
