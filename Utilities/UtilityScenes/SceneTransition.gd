302af1bf44901c724aa72428bcc99075ea44ca78
plexsoup <plexsoup@gmail.com>
2023-11-30 14:37:36 -0800

    connect StageManager to SceneTransition for fade-out / in

--- Utilities/UtilityScenes/SceneTransition.gd
+++ Utilities/UtilityScenes/SceneTransition.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 # Scene Transition method from this video: 
 # https://www.youtube.com/watch?v=yZQStB6nHuI&ab_channel=TheShaggyDev
+signal finished
 
 func change_scene(scene: PackedScene, anim_length: float = 0.5, hold: float = 0.0) -> void:
 	$AnimationPlayer.get_animation("dissolve").length = anim_length
@@ -10,4 +11,16 @@ func change_scene(scene: PackedScene, anim_length: float = 0.5, hold: float = 0.
 	if (hold > 0):
 		await get_tree().create_timer(hold).timeout
 	StageManager.change_scene_to(scene)
-	$AnimationPlayer.play_backwards("dissolve")
+	$AnimationPlayer.play("resolve")
+	
+
+
+func fade_out():
+	$AnimationPlayer.play("dissolve")
+	
+func fade_in():
+	$AnimationPlayer.play("resolve")
+	
+
+func _on_animation_player_animation_finished(anim_name):
+	finished.emit()
