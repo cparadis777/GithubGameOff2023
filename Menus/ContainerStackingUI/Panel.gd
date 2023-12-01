ff4b0562cd928088d4bf9c9c516aad6b8c2966bb
plexsoup <plexsoup@gmail.com>
2023-11-30 22:35:56 -0800

    wired more buttons

--- Menus/ContainerStackingUI/Panel.gd
+++ Menus/ContainerStackingUI/Panel.gd
@@ -1,5 +1,16 @@
 extends TextureRect
 
 
+func _ready():
+	connect_button_audio()
+	
+func connect_button_audio():
+	for button in get_children():
+		if button is Button or button is TextureButton:
+			button.pressed.connect($Audio/ClickNoise.play)
+			#button.mouse_entered.connect($Audio/HoverNoise.play)
+			
+	
 func _on_quit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
+
